06416f9bf2067a5eb7ffd77080c025c9024bf49a
DESKTOP-09RCJF3\maria <maria.dicu26@yahoo.com>
2023-04-12 12:59:02 +0300

    accesa internship

---
+++ GamificationAPI/GamificationAPI/Models/QuestDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GamificationAPI.Models
+{
+    public class QuestDbContext:DbContext
+    {
+        public QuestDbContext(DbContextOptions<QuestDbContext> options) : base(options)
+        { }
+        public DbSet<Question> Questions { get; set; }
+        public DbSet<Participant> Participants { get; set; }
+            
+    }
+}
